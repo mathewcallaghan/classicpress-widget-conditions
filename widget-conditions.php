34f4d1629b6fbdb563726df9ba36d5d5b5ec17c7
Mathew Callaghan <mathewcallaghan@gmail.com>
2021-06-17 11:07:08 +1000

    Update widget-conditions.php
    Removed GitHub Plugin URI

--- widget-conditions.php
+++ widget-conditions.php
@@ -13,7 +13,6 @@
  * Author URI:        https://mathew.callaghan.xyz/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
- * GitHub Plugin URI: mathewcallaghan/classicpress-widget-conditions
  * Text Domain:       cp-widget-conditions
  * Domain Path:       /languages
  */
